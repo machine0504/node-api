849abc4e29c568fbfbd8c2e17c6a63fa9168c2bd
buddy <544088599@qq.com>
2019-10-22 19:52:56 +0800

    node切换头像

---
+++ upload.js
@@ -0,0 +1,35 @@
+//node图片切换头像功能
+var express=require('express');
+var fs=require('fs');
+var path=require('path');
+var fm=require('formidable');
+var app=new express();
+app.use(express.static(path.join(__dirname,'public')));
+app.get('/',(req,res)=>{
+    res.header('Access-Control-Allow-Origin', '*');
+    console.log(req.url)
+    res.sendFile(path.join(__dirname,'public/upload.html'))
+    //res.sendFile('public/upload.html',{root:__dirname})
+    // 第四步:创建main.html文件
+})
+app.post('/ajaxUpload',(req,res)=>{
+    res.header('Access-Control-Allow-Origin', '*');
+    console.log(req.url)
+    var form=new fm.IncomingForm();
+    form.uploadDir=path.join(__dirname,'public/uploads');
+    // uploadDir设置文件的上传的路径
+    form.parse(req);
+    form.on('end',function(){
+        console.log('upload success')
+    })
+    form.on('file',function(field,file){
+        fs.renameSync(file.path,path.join(form.uploadDir,'/icon.png'))
+    })
+    res.send('./uploads/icon.png')
+})
+// app.listen(5000)
+var server=app.listen(5000,function () {
+    var host=server.address().address;
+    var port=server.address().port;
+    console.log("应用实例，访问地址为 http://%s:%s", host, port)
+})
